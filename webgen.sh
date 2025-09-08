7b04b015102ac2025a1efc25368834377a4afc90
Gaurang Parkar <gaurang@gaurangparkar.com>
2025-09-08 10:43:36 +0200

    update

---
+++ webgen.sh
@@ -0,0 +1,2 @@
+rm /Users/gaurang/files/webpage/gaurangparkar.github.io/.files
+/Users/gaurang/bin/ssg ../website /Users/gaurang/files/webpage/gaurangparkar.github.io 'test' 'https://www.gaurangparkar.com'
